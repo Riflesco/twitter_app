e040ec5365c2b33fd00ebc79e4536987e63c69f8
Riflesco <turmactibi@yahoo.com>
2022-04-07 21:50:37 +0300

    Main menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
     def alta_ruta
        # render html: 'Ai intrat pe alta ruta!'
     end
+
+    def create_user
+        #user = User.create(name: params[:bla]);
+       # render html: "Userul cu id: #{user.id} a fost creat"
+       #render html: "Pagina create user"
+    end
+    
+    def main_menu
+        
+    end
 end
