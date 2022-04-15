e09c2ec33da818d7d274f2563f8a8c568e4b6754
Riflesco <turmactibi@yahoo.com>
2022-04-15 15:59:25 +0300

    Add phone number column

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(name: "John" , email: "aa@aaa.com" , age: 22,gender: "Male")
-User.create(name: "Alexandra" , email: "alexandra@aaa.com" , age: 19,gender: "Female")
-User.create(name: "Mike" , email: "mike@aaa.com" , age: 18,gender: "Male")
-User.create(name: "Ioana" , email: "ioana@aaa.com" , age: 22,gender: "Female")
-User.create(name: "Andrei" , email: "andrei@aaa.com" , age: 25,gender: "Other")
+User.create(name: "John" , email: "aa@aaa.com" , age: 22,gender: "Male",phone_number: "0712345678")
+User.create(name: "Alexandra" , email: "alexandra@aaa.com" , age: 19,gender: "Female",phone_number: "0741215515")
+User.create(name: "Mike" , email: "mike@aaa.com" , age: 18,gender: "Male",phone_number: "078262333")
+User.create(name: "Ioana" , email: "ioana@aaa.com" , age: 22,gender: "Female",phone_number: "0745846699")
+User.create(name: "Andrei" , email: "andrei@aaa.com" , age: 25,gender: "Other",phone_number: "073629181")
